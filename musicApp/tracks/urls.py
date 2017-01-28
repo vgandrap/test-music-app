a34dec93532954954f77192d078ee62f01644709
Vijay Gandrapu <vijay.gandrapu@gmail.com>
2017-01-28 14:47:16 +0530

    base urls and views

--- musicApp/tracks/urls.py
+++ musicApp/tracks/urls.py
@@ -3,5 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.all_tracks, name='all_tracks'),
+    url(r'^add/?$', views.add_track, name='add_tracks'),
+    url(r'^(?P<track_id>[0-9]+)/?$', views.this_track, name='track'),
+    url(r'^(?P<track_id>[0-9]+)/edit/?$', views.edit_track, name='edit_track'),
+    url(r'^genre/?$', views.all_genres, name='all_genres'),
+    url(r'^genre/add?$', views.add_genre, name='add_genre'),
+    url(r'^genre/(?P<genre_id>[0-9]+)/?$', views.this_genre, name='genre'),
+    url(r'^genre/(?P<genre_id>[0-9]+)/edit/?$', views.edit_genre, name='edit_genre'),
 ]
--- musicApp/tracks/views.py
+++ musicApp/tracks/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world.")
+def all_tracks(request):
+    return HttpResponse("All Tracks")
+
+def add_track(request):
+    return HttpResponse("Add Tracks")
+
+def this_track(request, track_id):
+    return HttpResponse("This Track")
+
+def edit_track(request, track_id):
+    return HttpResponse("Edit Track")
+
+def all_genres(request):
+    return HttpResponse("All Genres")
+
+def add_genre(request):
+    return HttpResponse("Add Genres")
+
+def this_genre(request, genre_id):
+    return HttpResponse("This Genre")
+
+def edit_genre(request, genre_id):
+    return HttpResponse("Edit Genre")
