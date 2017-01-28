d6af5a83036e96159d7166bdd7b6e93a5b5f4a0a
Vijay Gandrapu <vijay.gandrapu@gmail.com>
2017-01-28 22:25:44 +0530

    Build the front end UI

---
+++ musicApp/tracks/static/js/global.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    // Toggle Navbar Active Links
+    // $("ul.nav.navbar-nav li").each(function() {
+    //     $(this).on('click', function() {
+    //         $("ul.nav.navbar-nav li").each(function() {
+    //             $(this).removeClass("active");
+    //         });
+    //         $(this).addClass("active");
+    //     });
+    // });
+
+    $("ul.nav.nav-pills li").each(function() {
+        $(this).on('click', function() {
+            $("ul.nav.nav-pills li").each(function() {
+                $(this).removeClass("active");
+            });
+            $(this).addClass("active");
+        });
+    });
+
+});
