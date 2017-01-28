c5251d71f4c69d7af05baf250472ce1da2a13cf5
Vijay Gandrapu <vijay.gandrapu@gmail.com>
2017-01-28 14:46:38 +0530

    Create basic models

--- musicApp/tracks/models.py
+++ musicApp/tracks/models.py
@@ -1,5 +1,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class Genre(models.Model):
+	genre_id = models.IntegerField(default=0, db_index=True, unique=True)
+	genre_type = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.genre_type
+
+@python_2_unicode_compatible
+class Track(models.Model):
+	title_id = models.IntegerField(default=0, db_index=True, unique=True)
+	title = models.CharField(max_length=200)
+	rating = models.FloatField()
+	# rating = models.DecimalField(max_digits=1, decimal_places=1)
+	genre = models.ForeignKey(Genre, to_field="genre_id")
+
+	def __str__(self):
+		return self.title
