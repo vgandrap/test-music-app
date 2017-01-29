6dbf4fa0bfad4e3548e9994a95e09ee83ba87c18
Vijay Gandrapu <vijay.gandrapu@gmail.com>
2017-01-29 20:19:19 +0530

    Scan API and get all genres and tracks to db

--- musicApp/tracks/views.py
+++ musicApp/tracks/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Genre, Track
 
 def all_tracks(request):
+    get_tracks()
     return render(request, 'tracks/base.html', {})
 
 def add_track(request):
@@ -24,3 +26,27 @@ def this_genre(request, genre_id):
 
 def edit_genre(request, genre_id):
     return HttpResponse("Edit Genre")
+
+def get_genres(url='http://104.197.128.152:8000/v1/genres'):
+    r=requests.get(url)
+    genres=r.json()
+
+    for genre in genres['results']:
+        obj, created = Genre.objects.get_or_create(genre_id=genre['id'], genre_type=genre['name'])
+
+    if genres['next']:
+        get_genres(genres['next'])
+
+def get_tracks(url='http://104.197.128.152:8000/v1/tracks'):
+    r=requests.get(url)
+    songs=r.json()
+
+    for song in songs['results']:
+        obj, created = Track.objects.get_or_create(title_id=song['id'], title=song['title'], rating=song['rating'])
+        if song['genres']:
+            for genre_item in song['genres']:
+                gen_obj, gen_created = Genre.objects.get_or_create(genre_id=genre_item['id'], genre_type=genre_item['name'])
+                obj.genre.add(Genre.objects.get(genre_id=genre_item['id']))
+
+    if songs['next']:
+        get_tracks(songs['next'])
