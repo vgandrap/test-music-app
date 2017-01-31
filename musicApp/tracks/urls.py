f1485bcb56c794699e7e9dc9391662dbf32b70f8
Vijay Gandrapu <vijay.gandrapu@gmail.com>
2017-01-31 20:34:41 +0530

    Add API Implementations

--- musicApp/tracks/urls.py
+++ musicApp/tracks/urls.py
@@ -4,12 +4,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^search/$', views.track_search, name='track_search'),
     url(r'^track_list/?$', views.tracks_list, name='tracks_list'),
-    url(r'^add/?$', views.add_track, name='add_tracks'),
-    url(r'^(?P<track_id>[0-9]+)/?$', views.this_track, name='track'),
+    url(r'^add/?$', views.add_track, name='add_track'),
+    url(r'^(?P<track_id>[0-9]+)/?$', views.this_track, name='this_track'),
     url(r'^(?P<track_id>[0-9]+)/edit/?$', views.edit_track, name='edit_track'),
     url(r'^genre/?$', views.genres_list, name='genres_list'),
-    url(r'^genre/add?$', views.add_genre, name='add_genre'),
+    url(r'^genre/add/?$', views.add_genre, name='add_genre'),
     url(r'^genre/(?P<genre_id>[0-9]+)/?$', views.this_genre, name='genre'),
     url(r'^genre/(?P<genre_id>[0-9]+)/edit/?$', views.edit_genre, name='edit_genre'),
 ]
--- musicApp/tracks/views.py
+++ musicApp/tracks/views.py
@@ -1,14 +1,20 @@
+import requests
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Genre, Track
-import requests
+import urllib
+from requests import get
+import json
 
 
 def home(request):
-    #    get_genres()
-    #    get_tracks()
-    return render(request, 'tracks/content_page.html', {})
+    # get_genres()
+    # get_tracks()
+    track_list = Track.objects.all()
+    genre_list = Genre.objects.all()
+
+    return render(request, 'tracks/content_page.html', {'genre_list': genre_list, 'track_list': track_list})
 
 
 def tracks_list(request):
@@ -27,15 +33,151 @@ def tracks_list(request):
 
 
 def add_track(request):
-    return HttpResponse("Add Tracks")
+    if request.POST:
+        response_to_frontend = {'message': 'Update failed'}
+        parameters = {}
+
+        for item in request.POST:
+            try:
+                parameters = json.loads(item)
+            except:
+                pass
+
+        json_data = {'title': parameters['track_title'], 'rating': parameters['track_rating'], 'genres': []}
+        if parameters.get('track_genre'):
+            for genre in parameters['track_genre']:
+                json_data['genres'].append(genre['id'])
+
+        api_url_path = 'http://104.197.128.152:8000/v1/tracks'
+
+        try:
+            json_headers = {'content-type': 'application/json'}
+            json_response = requests.post(
+                api_url_path,
+                data=json.dumps(json_data),
+                headers=json_headers
+            )
+
+            if json_response['reason']:
+                response_to_frontend['message'] = json_response['reason']
+            if json_response['status_code']:
+                response_to_frontend['status_code'] = json_response['status_code']
+            if json_response['id']:
+                response_to_frontend['id'] = json_response['id']
+                obj, created = Track.objects.update_or_create(title_id=json_response['id'], title=json_data['title'],
+                                                              rating=json_data['rating'])
+                if json_data['genres']:
+                    for genre_item in json_data['genres']:
+                        obj.genre.add(Genre.objects.get(genre_id=genre_item['id']))
+
+        except:
+            pass
+
+        return HttpResponse(
+            json.dumps(response_to_frontend),
+            status=200,
+            content_type='application/json'
+        )
+
+    return
+
+
+def track_search(request):
+    query = urllib.quote(request.GET.get('query').encode('utf8'))
+
+    search_url = 'http://104.197.128.152:8000/v1/tracks?title=' + query
+
+    r = get(search_url)
+    results = r.json()
+
+    track_results = []
+
+    for song in results['results']:
+        obj = {'title_id': song['id'], 'title': song['title'], 'rating': song['rating'], 'genre': []}
+        if song['genres']:
+            for genre_item in song['genres']:
+                g = {'genre_id': genre_item['id'], 'genre_type': genre_item['name']}
+                obj['genre'].append(g)
+        track_results.append(obj)
+
+    if results['next']:
+        get_tracks(results['next'])
+
+    paginator = Paginator(track_results, 10)
+    page = request.GET.get('page', 1)
+
+    try:
+        tracks = paginator.page(page)
+    except PageNotAnInteger:
+        tracks = paginator.page(1)
+    except EmptyPage:
+        tracks = paginator.page(paginator.num_pages)
+
+    return render(request, 'tracks/modal/search_result_modal.html', {'search_results': tracks})
 
 
 def this_track(request, track_id):
-    return HttpResponse("This Track")
+    track_object = Track.objects.get(title_id=track_id)
+    track_details = {'track_id': track_object.title_id, 'title': track_object.title, 'rating': track_object.rating}
+    genres = []
+    for g_item in track_object.genre.all():
+        g = {'genre_title': g_item.genre_type, 'genre_id': g_item.genre_id}
+        genres.append(g)
+
+    track_details['genre'] = genres
+
+    return JsonResponse(track_details)
 
 
 def edit_track(request, track_id):
-    return HttpResponse("Edit Track")
+    if request.POST:
+        response_to_frontend = {'message': 'Update failed'}
+        parameters = {}
+
+        for item in request.POST:
+            try:
+                parameters = json.loads(item)
+            except:
+                pass
+
+        json_data = {'id': parameters['track_id'], 'title': parameters['track_title'],
+                     'rating': parameters['track_rating'], 'genres': []}
+        if parameters.get('track_genre'):
+            for genre in parameters['track_genre']:
+                json_data['genres'].append(genre['id'])
+
+        api_url_path = 'http://104.197.128.152:8000/v1/tracks/' + track_id
+
+        try:
+            json_headers = {'content-type': 'application/json'}
+            json_response = requests.post(
+                api_url_path,
+                data=json.dumps(json_data),
+                headers=json_headers
+            )
+
+            if json_response['reason']:
+                response_to_frontend['message'] = json_response['reason']
+            if json_response['status_code']:
+                response_to_frontend['status_code'] = json_response['status_code']
+            if json_response['id']:
+                response_to_frontend['id'] = json_response['id']
+                obj, created = Track.objects.update_or_create(title_id=json_data['id'], title=json_data['title'],
+                                                              rating=json_data['rating'])
+                if json_data['genres']:
+                    for genre_item in json_data['genres']:
+                        obj.genre.add(Genre.objects.get(genre_id=genre_item['id']))
+
+        except:
+            pass
+
+        return HttpResponse(
+            json.dumps(response_to_frontend),
+            status=200,
+            content_type='application/json'
+        )
+
+    return
 
 
 def genres_list(request):
@@ -54,19 +196,99 @@ def genres_list(request):
 
 
 def add_genre(request):
-    return HttpResponse("Add Genres")
+    if request.POST:
+        response_to_frontend = {'message': 'Update failed'}
+        parameters = {}
+
+        for item in request.POST:
+            try:
+                parameters = json.loads(item)
+            except:
+                pass
+
+        json_data = {'name': parameters['track_title']}
+
+        api_url_path = 'http://104.197.128.152:8000/v1/genres'
+
+        try:
+            json_headers = {'content-type': 'application/json'}
+            json_response = requests.post(
+                api_url_path,
+                data=json.dumps(json_data),
+                headers=json_headers
+            )
+
+            if json_response['reason']:
+                response_to_frontend['message'] = json_response['reason']
+            if json_response['status_code']:
+                response_to_frontend['status_code'] = json_response['status_code']
+            if json_response['id']:
+                response_to_frontend['id'] = json_response['id']
+                Genre.objects.update_or_create(genre_id=json_response['id'], genre_type=json_data['name'])
+
+        except:
+            pass
+
+        return HttpResponse(
+            json.dumps(response_to_frontend),
+            status=200,
+            content_type='application/json'
+        )
+
+    return
 
 
 def this_genre(request, genre_id):
-    return HttpResponse("This Genre")
+    genre_object = Genre.objects.get(genre_id=genre_id)
+    genre_details = {'genre_id': genre_object.genre_id, 'title': genre_object.genre_type}
+    return JsonResponse(genre_details)
 
 
 def edit_genre(request, genre_id):
-    return HttpResponse("Edit Genre")
+    if request.POST:
+        response_to_frontend = {'message': 'Update failed'}
+        parameters = {}
+
+        for item in request.POST:
+            try:
+                parameters = json.loads(item)
+            except:
+                pass
+
+        json_data = {'id': parameters['genre_id'], 'name': parameters['track_title']}
+
+        api_url_path = 'http://104.197.128.152:8000/v1/genres' + genre_id
+
+        try:
+            json_headers = {'content-type': 'application/json'}
+            json_response = requests.post(
+                api_url_path,
+                data=json.dumps(json_data),
+                headers=json_headers
+            )
+
+            if json_response['reason']:
+                response_to_frontend['message'] = json_response['reason']
+            if json_response['status_code']:
+                response_to_frontend['status_code'] = json_response['status_code']
+            if json_response['id']:
+                response_to_frontend['id'] = json_response['id']
+                Genre.objects.update_or_create(genre_id=json_data['id'], genre_type=json_data['name'])
+
+        except:
+            pass
+
+        return HttpResponse(
+            json.dumps(response_to_frontend),
+            status=200,
+            content_type='application/json'
+        )
+
+    return
 
 
 def get_genres(url='http://104.197.128.152:8000/v1/genres'):
-    r = requests.get(url)
+    r = get(url)
     genres = r.json()
 
     for genre in genres['results']:
@@ -77,7 +299,7 @@ def get_genres(url='http://104.197.128.152:8000/v1/genres'):
 
 
 def get_tracks(url='http://104.197.128.152:8000/v1/tracks'):
-    r = requests.get(url)
+    r = get(url)
     songs = r.json()
 
     for song in songs['results']:
